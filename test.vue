0f3f78c7787e96cbce524584d7b6ef7dd39d0e7c
zzl <629834918@qq.com>
2023-04-11 12:55:26 +0800

    axios访问api

---
+++ test.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="send">发送</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+  methods: {
+    send() {
+      this.$axios({
+        method: "post",
+        url: "api/send_massage?user_code=2021010407&msg=hello",
+        // params: {
+        //   user_code: "2021010407",
+        //   msg: "hello world",
+        // },
+      }).then(
+        (res) => {
+          console.log(res.data);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    },
+  },
+};
+</script>
+
+<style>
+</style>
