c929da537e8322a3cab880aebe8ea3a937f4ccc4
zzl <629834918@qq.com>
2023-04-11 12:53:58 +0800

    配置代理

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,15 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   lintOnSave: false,
   transpileDependencies: true,
+  devServer: {
+    port: 5001,
+    proxy: {
+      "/api": {
+        target: "http://10.5.51.110:5001/",
+        pathRewrite: { "^/api": "" },
+        // ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
 });
